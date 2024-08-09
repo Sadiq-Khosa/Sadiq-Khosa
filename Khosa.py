1fd6cc82de73bc005c389475ac1fc168f66047da
Sadiq-Khosa <shreeavni0@gmail.com>
2024-08-09 17:03:21 +0500

    Update Khosa.py

--- Khosa.py
+++ Khosa.py
@@ -1,1 +1,7 @@
+import os,platform
+os.system('git pull')
+# exit(' Wait Tool On updating ')
+Khosa=platform.architecture()[0]
+if Khosa=="32bit":__import__("Khosa32")
+elif Khosa=="64bit":__import__("Khosa64")
 
